b119baa0e508e654f209bf1f5d51171d3a874f35
GabrielAgosu <gabrielagosu08@gmail.com>
2024-07-31 16:36:08 +0000

    javascript examples

---
+++ script.js
@@ -0,0 +1,34 @@
+
+  //console log text, expression 
+  console.log("hello world")
+  console.log(3+6)
+
+// Create a variable to hold your name as a string
+let myname= "gabe"
+myname ="moses"
+console.log(myname)
+
+const myMessage= "Good Morning,"
+
+console.log(myMessage+ myname)
+
+//Concatenate your name variable into a message and log it to the console.
+//Example: “Hello, Zach!” 
+
+//console log your name variable
+
+//Re-assign the value of your name variable below its declaration and console log it again
+
+//practice math operators
+
+// boolean - true or false
+
+//create null and undefined
+
+//mix data types
+
+//string methods
+
+//link up html file and create prompt and save value
+
+//access the DOM
